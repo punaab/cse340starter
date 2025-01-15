ee5c5cee972b5dc4f16acbdd84036b73400b53dd
punaab <notbitcoinceo@gmail.com>
2025-01-14 20:06:27 -0800

    Your commit message

--- server.js
+++ server.js
@@ -9,11 +9,22 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Index Route
+ *************************/
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
