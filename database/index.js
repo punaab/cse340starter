d9755a2e55876297df8a241fbbc21ec432ce12bf
punaab <notbitcoinceo@gmail.com>
2025-02-08 15:16:55 -0800

    added else pool to not reject

--- database/index.js
+++ database/index.js
@@ -7,11 +7,17 @@ require("dotenv").config()
  * If - else will make determination which to use
  * *************** */
 let pool
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl:  false
-  })
+    ssl: false,
+  });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, 
+  });
+}
 
 // Added for troubleshooting queries
 // during development
